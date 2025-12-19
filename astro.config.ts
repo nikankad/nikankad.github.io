324fc1d61f3fe29792dd78761edfa2184f3f6a00
Nikan <nikankad@protonmail.com>
2025-12-19 13:54:42 -0500

    init

---
+++ astro.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  output: "static",
+  prefetch: true,
+  compressHTML: true,
+  site: 'https://your-username.github.io',
+});
