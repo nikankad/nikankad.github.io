c0a744b9054c599ed1514eec2bd830a471c4ef78
Nikan <nikankad@protonmail.com>
2025-12-21 14:37:27 -0500

    updated blog

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
   output: "static",
   prefetch: true,
   compressHTML: true,
   site: 'https://nikankad.github.io',
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
